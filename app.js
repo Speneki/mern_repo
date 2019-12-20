0bc68a43fe7ae38d0823e6ffa843d2d9b50f8887
Spencer Tassone <stassone@nmef.com>
2019-12-20 15:57:47 -0500

    validations and routes init

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 

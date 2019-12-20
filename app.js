e7f0c299af7aaeceba9a848e31eb7f6ea6695974
Spencer Tassone <stassone@nmef.com>
2019-12-20 16:14:15 -0500

    validations

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Bonjourno");
 });
 app.use("/api/users", users);
 
